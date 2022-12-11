92bf40ce4b92c7cf93977453f906f44e496bb28f
Daria <DariaGN@yandex.ru>
2022-12-11 05:23:17 +0300

    second commit

--- lab_work_2/Matrix.h
+++ lab_work_2/Matrix.h
@@ -2,30 +2,40 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
+template <typename T>//, typename T2>
 class Matrix
 {
 private:
-	int columns, rows; // размер матрицы
+	int cols, rows; // размер матрицы
 	T* data; // массив элементов
 
 public:
 	Matrix<T>(int r, int c = 1) 
 	{
 		rows = r;
-		columns = c;
+		cols = c;
 		data = new T[r * c];
 	}
 
 	Matrix<T>(const Matrix& matrix) // copy constructor
 	{
 		rows = matrix.rows;
-		columns = matrix.columns;
-		data = new T[rows * columns];
-		for (int i = 0; i < rows*columns; i++)
+		cols = matrix.cols;
+		data = new T[rows * cols];
+		for (int i = 0; i < rows*cols; i++)
 		{
 			data[i] = matrix.data[i];
 		}
+	}
+
+
+	Matrix<T>(Matrix&& other) // move constructor
+	{
+		rows = other.rows;
+		cols = other.cols;
+		data = other.data;
+		other.data = nullptr;
+		other.rows = other.cols = 0;
 	}
 
 	// Matrix<T>({data1}, {})
@@ -33,12 +43,12 @@ public:
 	// }
 
 	Matrix<T> operator = (const Matrix<T> matrix) {		// пока только для одинаковых типов
-		if (rows * columns < matrix.rows * matrix.columns) {
-			data = new T[matrix.rows * matrix.columns];
+		if (rows * cols < matrix.rows * matrix.cols) {
+			data = new T[matrix.rows * matrix.cols];
 		}
 		rows = matrix.rows;
-		columns = matrix.columns;
-		for (int i = 0; i < rows * columns; i++)
+		cols = matrix.cols;
+		for (int i = 0; i < rows * cols; i++)
 		{
 			data[i] = matrix.data[i];
 		}
@@ -50,9 +60,9 @@ public:
 	{
 		for (int i = 0; i < matrix.rows; i++)
 		{
-			for (int j = 0; j < matrix.columns; j++)
+			for (int j = 0; j < matrix.cols; j++)
 			{
-				in >> matrix.data[i * matrix.columns + j];
+				in >> matrix.data[i * matrix.cols + j];
 			}
 		}
 		return in;
@@ -62,16 +72,120 @@ public:
 	{
 		for (int i = 0; i < matrix.rows; i++)
 		{
-			for (int j = 0; j < matrix.columns; j++)
+			for (int j = 0; j < matrix.cols; j++)
 			{
-				out << matrix.data[i * matrix.columns + j]<<' ';
+				out << matrix.data[i * matrix.cols + j]<<' ';
 			}
 			out << endl;
 		}
 		return out;
 	}
 
+	T operator () (int r, int c) { // не сделано для m(0,2) = 5
+		return data[r * cols + c];
+	}
 
+
+	Matrix<T> operator + ( const Matrix<T> summand)  // сложение матриц
+	{
+		if (rows != summand.rows || cols != summand.cols)
+		{
+			cout << "the sizes of the matrices do not match\n";
+			return Matrix<T> (0,0);
+		}
+		Matrix <T> summ(rows, cols); 
+		for (int i = 0; i < rows*cols; i++)
+		{
+			summ.data[i] = data[i] + summand.data[i];
+		}
+		return summ;
+	}
+
+	Matrix<T> operator - (const Matrix<T> a) // вычитание матриц 
+	{
+		if (rows != a.rows || cols != a.cols)
+		{
+			cout << "the sizes of the matrices do not match\n";
+			return Matrix<T>(0, 0);
+		}
+		Matrix <T> minus(rows, cols);
+		for (int i = 0; i < rows * cols; i++)
+		{
+			minus.data[i] = data[i] - a.data[i];
+		}
+		return minus;
+	}
+
+	Matrix<T> operator *(const T n)   // умножение на число
+	{
+		Matrix <T> m(rows, cols);
+		for (int i = 0; i < rows * cols; i++)
+		{
+			m.data[i] = data[i] * n;
+		}
+		return m;
+	}
+
+	Matrix <T> operator * (const Matrix<T> b)   // умножение матриц
+	{
+		if (cols != b.rows) {
+		cout << "incorrect sizes of the matrices \n";
+		return Matrix<T>(0, 0);
+		}
+		Matrix<T> c(rows, b.cols); // матрица произведения
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < b.cols; j++)
+			{
+				c.data[i * b.cols + j] = 0;
+				for (int k = 0; k < cols; k++)
+				{
+					c.data[i * b.cols + j] += data[i * cols + k] * b.data[k * b.cols + j];
+				}
+			}
+		}
+		return c;
+	}
+
+	Matrix <T> operator += (const Matrix<T> summand)
+	{
+		if (rows != summand.rows || cols != summand.cols)
+		{
+			cout << "the sizes of the matrices do not match\n";
+			return Matrix<T>(0, 0);
+		}
+		
+		for (int i = 0; i < rows * cols; i++)
+		{
+			data[i] += summand.data[i];
+		}
+
+		return *this;
+	}
+
+	Matrix <T> operator -= (const Matrix<T> a)
+	{
+		if (rows != a.rows || cols != a.cols)
+		{
+			cout << "the sizes of the matrices do not match\n";
+			return Matrix<T>(0, 0);
+		}
+		for (int i = 0; i < rows * cols; i++)
+		{
+			data[i] -= a.data[i];
+		}
+		return *this;
+	}
+
+	Matrix <T> operator *= (const T n)
+	{
+		for (int i = 0; i < rows * cols; i++)
+		{
+			data[i] = data[i] * n;
+		}
+		return *this;
+	}
+	 
 	~Matrix()
 	{
 		delete[] data;
--- lab_work_2/lab_work_2.cpp
+++ lab_work_2/lab_work_2.cpp
@@ -3,18 +3,49 @@
 int main() {
 
 	Matrix<int> m(2, 3);
+
+	cout << "enter matrix m: \n";
 	cin >> m;
 	cout << "matrix m:\n" << m;
-	Matrix <int> m1(4); // матрица вида: 4 строки и 1 столбец
-	Matrix <int> m2(4, 6); // матрица вида: 4 строки и 6 столбцов
-	Matrix <int> m3(m);  // copy constructor
-	cout << "matrix m3 (copy constructor Matrix <int> m3(m)):\n" << m3;
-	m2 = m;
-	cout << "matrix m2 (operator = (m2 = m)):\n"<< m2;
+	cout << "enter matrix summand: \n";
+	Matrix<int> summand(2, 3);
+	cin >> summand;
+	cout << "matrix summand:\n" << summand;
+	//Matrix <int> m1(4); // матрица вида: 4 строки и 1 столбец
+	//Matrix <int> m2(4, 6); // матрица вида: 4 строки и 6 столбцов
+	//Matrix <int> m3(m);  // copy constructor
+	//cout << "matrix m3 (copy constructor Matrix <int> m3(m)):\n" << m3;
+	//m2 = m;
+	//cout << "matrix m2 (operator = (m2 = m)):\n"<< m2;
 	//Matrix <int> m5 = { {1, 2, 3}, {4, 5, 6} };
 	//Matrix <int> m6 = { {1, 2, 3, 4, 5, 6} };
 	//Matrix <int> m7 = { 1,2,3,4,5,6 };
 	//Matrix <int> m8 = { {1},{2},{3},{4},{5},{6} };
+	// 
+	// Перемещающий конструктор
+	//Matrix <int> m4 = move(m2);
+
+	//cout << "matrix m2 (move constructor):\n" << m2;
+	//cout << "matrix m4 (move constructor):\n" << m4;
+
+	//cout << "m4(0,2) = " << m4(0, 2) << endl;
+	//Matrix<int> sum = m + summand;
+	//cout << "matrix sum = m + summand:\n" << sum;
+
+
+	//Matrix<int> minus = m1 - summand;
+	//cout << "matrix minus = sum - summand:\n" << minus;
+	//m4(0, 2) = 7;
+
+	//Matrix<int> compose = m * summand;
+	//cout << "compose = m * summand: \n" << compose << endl;
+
+	m += summand;
+	cout << "m += summand:\n" << m << endl;
+	m -= summand;
+	cout << "m -= summand:\n" << m << endl;
+	m *= 4;
+	cout << "m *= 4:\n" << m << endl;
 
 	return 0;
 }
